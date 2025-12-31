f3595a002e05d452ae5a6d6304968135f1fd33c1
Dima <kapustin.dmitry@gmail.com>
2025-12-31 19:52:04 +0300

    .

--- tui++tests/Src/test_Action.cpp
+++ tui++tests/Src/test_Action.cpp
@@ -6,7 +6,7 @@
 using namespace tui;
 
 class TestAction: public Action {
-  Property<bool> enabled { this, "enabled" };
+  Property<bool> enabled { this, "enabled", false };
 public:
   virtual bool is_enabled() const override {
     return this->enabled;
@@ -29,17 +29,24 @@ void test_Action() {
   auto action = TestAction { };
   action.add_property_change_listener([](PropertyChangeEvent &e) {
     if (e.property_name == Action::NAME) {
+      assert(std::any_cast<std::string>(e.old_value) == "");
       assert(std::any_cast<std::string>(e.new_value) == NAME);
     } else if (e.property_name == Action::SHORT_DESCRIPTION) {
+      assert(std::any_cast<std::string>(e.old_value) == "");
       assert(std::any_cast<std::string>(e.new_value) == SHORT_DESCRIPTION);
     } else if (e.property_name == Action::MNEMONIC) {
+      assert(std::any_cast<Char>(e.old_value) == '\0');
       assert(std::any_cast<Char>(e.new_value) == MNEMONIC);
+    } else if (e.property_name == "enabled") {
+      assert(std::any_cast<bool>(e.old_value) == false);
+      assert(std::any_cast<bool>(e.new_value) == true);
     }
   });
 
   action.set_name(NAME);
   action.set_short_description(SHORT_DESCRIPTION);
   action.set_mnemonic(MNEMONIC);
+  action.set_enabled(true);
 
   assert(action.get_name() == NAME);
   assert(action.get_short_description() == SHORT_DESCRIPTION);
